b1dc2028bb7be710a2780fee06f14348d624390a
fxs666 <2541100709@qq.com>
2019-10-24 21:54:16 +0800

    practice

---
+++ STL/STL/test.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+#include<vector>
+using namespace std;
+
+
+//int main()
+//{
+//	string s("hello world!");
+//	//cout << s.size() << endl;
+//	/*cout << s.length() << endl;
+//	cout << s.capacity() << endl;
+//	s.reserve(100);
+//	cout << s.capacity() << endl;*/
+//
+//	/*s.resize(5);
+//	cout << s.size() << endl;
+//	cout << s.capacity() << endl;*/
+//
+//	s.reserve(100);
+//	cout << s.size() << endl;
+//	s.resize(100);
+//	cout << s.size() << endl;
+//
+//	cout << s.capacity() << endl;
+//
+//
+//	system("pause");
+//	return 0;
+//}
+
+
+//总结当将有效的字符变小的时候，或者预留的空间小于string对象本身的时候，string本身的容量大小不会改变。
+//但是比本身容量大的话，就会改变
+
+
+
+int main()
+{
+	/*string name;
+	getline(cin, name);
+	cout << name << endl;*/
+
+	std::vector<char>  name;
+
+
+
+	system("pause");
+	return 0;
+}
