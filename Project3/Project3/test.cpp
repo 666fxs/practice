b356e4862ed1dab84fbc60f0e02bfaca5a3a1289
fxs666 <2541100709@qq.com>
2019-10-29 21:04:34 +0800

    practice

---
+++ Project3/Project3/test.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+//int f(int x)
+//{
+//   
+//
+//	if (x <= 2)
+//	{
+//		return 1;
+//	}
+//	count++;
+//	return f(x - 2) + f(x - 4,count) + 1;
+//}
+//int main()
+//{
+//	int count = 0;
+//	f(10,count);
+//	cout << count << endl;
+//	system("pause");
+//
+//}
+//
+//int main()
+//{
+//	int count = 0;
+//	for (int n = -1; n != 0; n++)
+//	{
+//		count++;
+//	}
+//	cout << count << endl;
+//	system("pause");
+//	return 0;
+//}
+
+int f(int n)
+{
+
+	if (n == 0)
+	{
+		return 1;
+	}
+}
+
+int main()
+{
+
+	return 0;
+}
---
+++ 类/类/test.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class Time
+{
+
+
+public:
+	Time()
+	{
+		Hour = 2018;
+		Minite = 10;
+		Second = 25;
+	}
+
+public:
+	int Hour;
+	int Minite;
+	int sencod;
+
+};
+
+
+
+int main()
+{
+
+}
