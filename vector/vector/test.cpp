561f3fb47a18007c414cedea1d711b9cab1d5d87
fxs666 <2541100709@qq.com>
2019-11-09 21:10:32 +0800

    pratice

---
+++ vector/vector/test.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	//vector<int> v1(2);
+	//v1.push_back(10);
+	//v1.push_back(20);
+	//v1.push_back(30);
+	//v1.push_back(40);
+	//v1.push_back(50);
+	//v1.push_back(60);
+	//v1.push_back(70);
+	//v1.push_back(70);
+	//v1.push_back(70);
+	//v1.push_back(70);
+
+	//cout <<v1[0] << endl;
+	//cout << &(v1.begin()) << endl;
+	//cout << *(v1.cbegin()+2) << endl;
+
+
+
+	system("pause");
+	return 0;
+}
