f8ae3928ccc6e7b25071a5def29ef4ae8932e90c
fxs666 <2541100709@qq.com>
2019-11-17 18:03:58 +0800

    pratice

---
+++ reverse/reverse/test.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<string>
+#include <algorithm>
+#include<cstring>
+using namespace std;
+
+
+
+int main()
+{
+	//1、翻转一个字符串
+	//dlrow olleh
+	/*string s = "hello world";
+	reverse(s.begin(),s.end());
+	cout << s << endl;*/
+	
+	//2、翻转一个字符串
+	/*string s = "helloworld";
+	int size = s.size() - 1;
+	for (int i = 0;i <= size / 2;i++)
+	{
+		char tmp = s[i];
+		s[i] = s[size - i];
+		s[size- i] = tmp;
+	}
+    
+	cout << s << endl;
+	*/
+	
+	//3、翻转一句话
+	//源：ni hao zhong guo
+	//目：guo zhong hao ni
+	//string s;
+	//getline(cin, s);
+	//reverse(s.begin(), s.end());
+	//string::iterator itea1,itea2;
+	//itea1 = s.begin();
+	//itea2 = s.begin();
+	//while (1)
+	//{
+	//	if (*itea2 == ' '||*itea2=='\n')
+	//	{
+	//		reverse(itea1, itea2);
+	//		itea1 = itea2;
+	//		if (*itea2 == '\n')
+	//		{
+	//			break;
+	//		}
+	//	}
+	//	
+	//	itea2++;
+	//}
+
+	//cout << s << endl;
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
