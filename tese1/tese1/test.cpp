8315476f9145a162646d7db7a1c0590e8a1acec0
fxs666 <2541100709@qq.com>
2019-11-19 19:22:39 +0800

    pratice

---
+++ tese1/tese1/test.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class A
+{
+public:
+	A(){
+		cout << "A" << endl;
+	}
+};
+
+int main()
+{
+	A arr[3];
+	system("pause");
+	return 0;
+}
---
+++ 输出/输出/test.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	/////*char c;
+	////c = 97;
+	////printf("%c", c);*/
+	////double c = 1234.1234;
+	//printf("%1.1f", c);
+
+
+
+	int a = 1;
+	switch (a)
+	{
+		case 1:
+			printf("1\n");
+		case 2:
+			printf("2\n");
+		case 3:
+			printf("3\n");
+		case 4:
+			printf("4\n");
+		default:
+			printf("meiyou!\n");
+
+
+	}
+
+
+
+	system("pause");
+	return 0;
+}
